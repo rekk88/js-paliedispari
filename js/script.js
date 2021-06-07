85c1a2758b43c81351e488c08bcc01b162a95677
maicol <maicol.matteoli@gmail.com>
2021-06-07 14:17:35 +0200

    test function palindroma() con passaggio di parametri

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var str = "ciao";
+
+document.getElementById("palindroma").addEventListener("click",function(){
+    palindroma(str);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------funcions--------------
+
+function palindroma(stringa){
+    alert(stringa);
+}
